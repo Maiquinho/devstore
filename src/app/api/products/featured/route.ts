2ed0631ef8841f3b430041e6a821d0480ab1a9f9
Michael Matheus <michaelmatheus2001@gmail.com>
2024-05-30 17:15:37 -0300

    Route Handlers no Next.js

---
+++ src/app/api/products/featured/route.ts
@@ -0,0 +1,7 @@
+import data from '../data.json'
+
+export async function GET() {
+  const featuredData = data.products.filter((product) => product.featured)
+
+  return Response.json(featuredData)
+}
